9d20c1b2c7c57b450840c289b523e3421e7bdff7
Joy <joy.yang@sailheader.cn>
2024-06-03 20:33:40 +0800

    feat:调试环境搭好

---
+++ spring-boot-samples/spring-boot-sample-tomcat/src/main/java/sample/tomcat/bean/LinuxEnvironment.java
@@ -0,0 +1,29 @@
+package sample.tomcat.bean;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Joy
+ * @date 2024/6/3
+ * @param
+ *
+ */
+
+
+public class LinuxEnvironment implements Serializable {
+
+	private String name;
+
+	public LinuxEnvironment() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
